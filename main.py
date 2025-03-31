47ce8d7b02c2c477ae6fa36cacae459af9c2bc6d
morpheus17201 <morpheus17201@gmail.com>
2025-03-31 15:12:46 +0530

    creating main.py

---
+++ main.py
@@ -0,0 +1,88 @@
+from fastapi import FastAPI, File, UploadFile, Form, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from typing import Optional, List
+import httpx
+
+# from openai_client import get_openai_response
+from file_handler import save_upload_file_temporarily
+
+from base_logger import logger
+
+# Import the functions you want to test directly
+# from functions import *
+
+app = FastAPI(title="IITM Assignment API")
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+endpoint_URL = r"https://b6fe-49-36-103-228.ngrok-free.app"
+
+
+@app.post("/")
+@app.post("/api/")
+async def process_question(
+    # question: str = Form(...), file: Optional[UploadFile] = File(None)
+    question: str = Form(...),
+    file: List[UploadFile] = File([]),
+):
+    print(" " * 80)
+    print("=" * 80)
+    print(" " * 80)
+
+    logger.info(f"Received question: {question}")
+    logger.info(f"Number of files received: {len(file)}")
+    try:
+        # Save file temporarily if provided
+        logger.info(f"Attempting to save the file")
+        file_path_list = []
+        temp_file_path = None
+        files_dict = {}
+        if file:
+            logger.info(f"Detected that file has been received")
+            if len(file) == 1:
+                logger.info(f"Only single file received")
+                logger.info(f"Attempting to save the file")
+                temp_file_path = await save_upload_file_temporarily(file[0])
+                logger.info(f"Successfully saved the file to {temp_file_path}")
+                files_dict = {file[0].filename: open(temp_file_path, "rb")}
+
+            elif len(file) > 1:
+                file_path_list = []
+                logger.info(f"Multiple files received")
+                logger.info(f"Attempting to save multiple files")
+                for counter, f in enumerate(file):
+                    logger.info(f"Saving file {counter} of {len(file)}")
+                    temp_file_path = await save_upload_file_temporarily(f)
+                    logger.info(f"Saved succesfully to {temp_file_path}")
+                    file_path_list.append(temp_file_path)
+                    files_dict = {f.filename: open(temp_file_path, "rb")}
+
+        # Get answer from OpenAI
+        async with httpx.Client() as client:
+            logger.info(f"Sending question & files to endpoint {endpoint_URL}")
+            answer = await client.post(
+                url=endpoint_URL, files=files_dict, data=question
+            )
+
+        # answer = await get_openai_response(question, temp_file_path)
+
+        return {"answer": answer}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    logger.info("Starting FastAPI server...")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    # uvicorn.run("app", host="0.0.0.0", port=8000, reload=True)
